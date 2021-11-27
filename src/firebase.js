6840c1cbffd07da2682b8d9bc9767911a95d007f
Shankar Ganesh <shankar.ganesh@seamoney.com>
2021-11-28 03:32:03 +0800

    firebase deployed

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase/compat";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyArH0JK6_JxoPz87l6W5tUKGNv3l61HRdk",
-    authDomain: "sai-clone.firebaseapp.com",
-    projectId: "sai-clone",
-    storageBucket: "sai-clone.appspot.com",
-    messagingSenderId: "507919351259",
-    appId: "1:507919351259:web:95a2c7f4d1954ae135bbfe",
-    measurementId: "G-3WJCG2FW2V",
+  apiKey: "AIzaSyCmlPSwwrcjWEkPNRKmwu_mNXs0r5PI-Qw",
+  authDomain: "ecommerce-4d68b.firebaseapp.com",
+  projectId: "ecommerce-4d68b",
+  storageBucket: "ecommerce-4d68b.appspot.com",
+  messagingSenderId: "818435864653",
+  appId: "1:818435864653:web:f96579ac1ca2ad022b8404",
+  measurementId: "G-XLM5VPZWL1",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore();
+const db = app.firestore();
 const auth = firebase.auth();
 
-export {db, auth};
+export { db, auth };
